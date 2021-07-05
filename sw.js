4f6deb71771ba64ca6db3b754075c8eecc3a3441
footfoot <75483966+footfoot-bit@users.noreply.github.com>
2021-07-05 22:58:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,22 @@
 // Files to cache
-// const cacheName = 'fafantoApp';
-// const appShellFiles = [
-//   'index.html',
-//   'fafanto.js',
-//   'fafanto.css',
-//   'favicon.svg',
-//   'favicon.png',
-//   'default-template.html',
-//   'default-style.html',
-//   'default-main.js',
-// ];
+const cacheName = 'fafantoApp';
+const appFiles = [
+  'index.html',
+  'fafanto.js',
+  'fafanto.css',
+  'favicon.svg',
+  'favicon.png',
+  'default-template.html',
+  'default-style.html',
+  'default-main.js',
+];
 // Installing Service Worker
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil((async () => {
-    // const cache = await caches.open(cacheName);
+    const cache = await caches.open(cacheName);
     console.log('[Service Worker] Caching all: app shell and content');
-    // await cache.addAll(contentToCache);
+    await cache.addAll(appFiles);
   })());
 });
 
