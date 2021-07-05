5408eaad0c64c2937aa919c8be3f1e85ad97a753
footfoot <75483966+footfoot-bit@users.noreply.github.com>
2021-07-05 20:55:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,14 +11,14 @@
 //   'default-main.js',
 // ];
 // Installing Service Worker
-// self.addEventListener('install', (e) => {
-//   console.log('[Service Worker] Install');
-//   e.waitUntil((async () => {
-//     const cache = await caches.open(cacheName);
-//     console.log('[Service Worker] Caching all: app shell and content');
-//     await cache.addAll(contentToCache);
-//   })());
-// });
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    // const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    // await cache.addAll(contentToCache);
+  })());
+});
 
 // Fetching content using Service Worker
 // self.addEventListener('fetch', (e) => {
